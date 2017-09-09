a34863e1e46769ed13674778396790212cc29f96
Kenneth Leung <ccllnekk@gmail.com>
2017-09-08 18:57:02 -0700

    kl

--- x.js
+++ x.js
@@ -1,2 +1,8 @@
 /* Kirby Generated: Test only */
--->>((  ),(),());
+((!poo(3)) ?
+    (function() {
+  return (2 + 3);
+  })() :
+  undefined);
+poo(["hello","shit"].join(''),333);
+(5 / (4 * (2 + 3)));
