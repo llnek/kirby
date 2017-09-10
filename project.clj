b38166e37c6fe5f8ddfdc41ad784a9a7d6f34c2c
Kenneth Leung <ccllnekk@gmail.com>
2017-09-10 00:14:49 -0700

    basically works

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject kirby "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject kirby "1.0.0"
+  :description ""
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
